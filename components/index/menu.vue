955c7660cecca13bde8b0ad0ee843f2df56f59e0
bingfeng yu <ybf0711@gmail.com>
2020-01-07 14:25:05 +0800

    7-7 首页菜单

---
+++ components/index/menu.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="m-menu">
+    <div class="nav">
+      <dt>全部分类</dt>
+      <dd
+        v-for="(item, index) in menu"
+        :key="index"
+      >
+        <i :class="item.type" />{{item.name}}<span class="arrow" />
+      </dd>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      menu: [{
+        type: 'food',
+        name: '美食'
+      },
+      {
+        type: 'takeout',
+        name: '外卖'
+      }]
+    }
+  },
+}
+</script>
+
+<style lang="scss">
+ @import "@/assets/css/index/index.scss";
+</style>
