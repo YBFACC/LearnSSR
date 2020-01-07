2e0ea366550bb5cdd63f9f4646f3294945425005
bingfeng yu <ybf0711@gmail.com>
2020-01-07 15:43:15 +0800

    7-8

--- components/index/menu.vue
+++ components/index/menu.vue
@@ -1,33 +1,104 @@
 <template>
   <div class="m-menu">
-    <div class="nav">
+    <dl
+      class="nav"
+      @mouseleave="mouseleave"
+    >
       <dt>全部分类</dt>
       <dd
+        @mouseenter="enter"
         v-for="(item, index) in menu"
         :key="index"
       >
-        <i :class="item.type" />{{item.name}}<span class="arrow" />
+        <i :class="item.type" />{{ item.name }}<span class="arrow" />
       </dd>
+    </dl>
+
+    <div
+      class="detail"
+      v-if="kind"
+      @mouseenter="sover"
+      @mouseleave="sout"
+    >
+      <template v-for="(item, index) in curdetail.child">
+        <h4 :key="index">{{ item.title }}</h4>
+        <span
+          v-for="(child, index) in item.child"
+          :key="index"
+        >{{
+          child
+        }}</span>
+      </template>
     </div>
   </div>
 </template>
+
+
 <script>
 export default {
   data () {
     return {
-      menu: [{
-        type: 'food',
-        name: '美食'
-      },
-      {
-        type: 'takeout',
-        name: '外卖'
-      }]
+      kind: "",
+      menu: [
+        {
+          type: "food",
+          name: "美食",
+          child: [
+            {
+              title: "美食",
+              child: ["1111", "2222", "33333"]
+            }
+          ]
+        },
+        {
+          type: "takeout",
+          name: "外卖",
+          child: [
+            {
+              title: "外卖",
+              child: ["美团外卖"]
+            }
+          ]
+        },
+        {
+          type: "hotel",
+          name: "酒店",
+          child: [
+            {
+              title: "酒店星级",
+              child: ["1111", "2222", "33333"]
+            }
+          ]
+        }
+      ]
+    };
+  },
+  computed: {
+    curdetail () {
+      return this.menu.filter(item => item.type === this.kind)[0];
     }
   },
-}
+  methods: {
+    mouseleave () {
+      let self = this;
+      console.log(self);
+      self._timer = setTimeout(() => {
+        self.kind = "";
+      }, 150);
+    },
+    enter (e) {
+      this.kind = e.target.querySelector('i').className
+    },
+    sover(){
+      clearTimeout(this._timer)
+    },
+    sout(){
+      this.kind=''
+    }
+  }
+};
 </script>
 
 <style lang="scss">
- @import "@/assets/css/index/index.scss";
+@import "@/assets/css/index/index.scss";
 </style>
