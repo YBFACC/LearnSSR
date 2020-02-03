a3dd0b602ff0b07aeaa9bace3c2eb2085ac499b6
bingfeng yu <ybf0711@gmail.com>
2020-02-03 22:20:34 +0800

    8-8 实现登录

--- server/interface/users.js
+++ server/interface/users.js
@@ -130,8 +130,8 @@ router.post("/verify", async (ctx, next) => {
   let mailOptions = {
     from: `"认证邮件" <${Email.smtp.user}>`,
     to: ko.email,
-    subject: "《慕课网高仿美团网全栈实战》注册码",
-    html: `您在《慕课网高仿美团网全栈实战》课程中注册，您的邀请码是${ko.code}`
+    subject: "YBF开发测试邮件",
+    html: `发送成功，您的邀请码是${ko.code}`
   };
   await transporter.sendMail(mailOptions, (error, info) => {
     if (error) {
